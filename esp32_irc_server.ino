a2de2716cf369eaecf580b87eb47049f79074077
Leonardo Galves <leonardosgalves@gmail.com>
2023-04-24 06:45:12 -0300

    Change code style

--- esp32_irc_server.ino
+++ esp32_irc_server.ino
@@ -25,27 +25,27 @@
 
 TFT_eSPI tft = TFT_eSPI();
 TFT_eSprite background = TFT_eSprite(&tft);
-TFT_eSprite logoSprite = TFT_eSprite(&tft);
-TFT_eSprite checkSprite = TFT_eSprite(&tft);
+TFT_eSprite logo_sprite = TFT_eSprite(&tft);
+TFT_eSprite check_sprite = TFT_eSprite(&tft);
 
 WiFiServer server(IRC_PORT);
 
-int logoX = 106;
+int logo_x = 106;
 int pulse = 7;
-int pulseClean = 0;
+int pulse_clean = 0;
 int clients = 0;
 String channels[] = {"example"};
 
-String getMemoryUsage() {
-  size_t totalHeapSize = esp_get_minimum_free_heap_size() + esp_get_free_heap_size();
-  size_t freeHeapSize = esp_get_free_heap_size();
-  float percMemUsage = 100 - (((float)freeHeapSize / (float)totalHeapSize) * 100);
-  static char memUsage[10];
-  snprintf(memUsage, sizeof(memUsage), "%.2f%%", percMemUsage);
-  return memUsage;
+String get_memory_usage() {
+  size_t total_heap_size = esp_get_minimum_free_heap_size() + esp_get_free_heap_size();
+  size_t free_heap_size = esp_get_free_heap_size();
+  float perc_cem_usage = 100 - (((float)free_heap_size / (float)total_heap_size) * 100);
+  static char mem_usage[10];
+  snprintf(mem_usage, sizeof(mem_usage), "%.2f%%", perc_cem_usage);
+  return mem_usage;
 }
 
-void connectWifi() {
+void wifi_connect() {
   WiFi.begin(WIFI_SSID, WIFI_PASS);
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -58,38 +58,38 @@ void connectWifi() {
   Serial.println("Server started");
 }
 
-void splashScreen() {
+void splash_screen() {
   background.createSprite(DISPLAY_W, DISPLAY_H);
   background.setSwapBytes(true);
-  logoSprite.createSprite(109, 63);
+  logo_sprite.createSprite(109, 63);
   background.fillSprite(TFT_WHITE);
-  logoSprite.pushImage(0, 0, 109, 63, logo);
-  logoSprite.pushToSprite(&background, logoX, 54, TFT_WHITE);
+  logo_sprite.pushImage(0, 0, 109, 63, logo);
+  logo_sprite.pushToSprite(&background, logo_x, 54, TFT_WHITE);
   background.pushSprite(0, 0);
   delay(2000);
 
-  while (logoX > 25) {
-    logoX-=3;
+  while (logo_x > 25) {
+    logo_x -= 3;
     background.fillSprite(TFT_WHITE);
-    logoSprite.pushToSprite(&background, logoX, 54, TFT_WHITE);
+    logo_sprite.pushToSprite(&background, logo_x, 54, TFT_WHITE);
     background.pushSprite(0, 0);
   }
   delay(1000);
 
-  int nameLength = strlen(APP_NAME);
-  char nameBuffer[nameLength + 1];
-  nameBuffer[0] = '\0';
+  int name_length = strlen(APP_NAME);
+  char name_buffer[name_length + 1];
+  name_buffer[0] = '\0';
 
   tft.setTextColor(APP_BLACK, APP_BLACK);
-  for (int i = 0; i < nameLength; i++) {
-    strncat(nameBuffer, &APP_NAME[i], 1);
-    tft.drawString(nameBuffer, 140, 70, 4);
+  for (int i = 0; i < name_length; i++) {
+    strncat(name_buffer, &APP_NAME[i], 1);
+    tft.drawString(name_buffer, 140, 70, 4);
     delay(300);
   }
   tft.drawString("IRC SERVER", 181, 95, 2);
 }
 
-void dashboard(){
+void dashboard() {
   String ip = "0.0.0.0";
   int conn = 0;
   if (WiFi.status() == WL_CONNECTED) {
@@ -103,34 +103,34 @@ void dashboard(){
 
   if (pulse < 18) {
     pulse++;
-  } else if (pulseClean < 18) {
-    pulseClean++;
+  } else if (pulse_clean < 18) {
+    pulse_clean++;
   } else {
     pulse = 7;
-    pulseClean = 0;
+    pulse_clean = 0;
   }
-  uint16_t pulseColor = conn ? APP_LGREEN : APP_LRED;
-  uint16_t connColor = conn ? APP_GREEN : APP_RED;
+  uint16_t pulse_color = conn ? APP_LGREEN : APP_LRED;
+  uint16_t conn_color = conn ? APP_GREEN : APP_RED;
 
-  background.fillCircle(23, 23, pulse, pulseColor);
-  background.fillCircle(23, 23, pulseClean, APP_WHITE);
-  checkSprite.createSprite(20, 20);
-  checkSprite.pushImage(0, 0, 20, 20, conn ? check : error);
-  checkSprite.pushToSprite(&background, 13, 13, TFT_BLACK);
+  background.fillCircle(23, 23, pulse, pulse_color);
+  background.fillCircle(23, 23, pulse_clean, APP_WHITE);
+  check_sprite.createSprite(20, 20);
+  check_sprite.pushImage(0, 0, 20, 20, conn ? check : error);
+  check_sprite.pushToSprite(&background, 13, 13, TFT_BLACK);
 
   background.setTextColor(TFT_BLACK, TFT_BLACK);
   background.drawString(ip, 46, 10, 4);
-  background.setTextColor(connColor, connColor);
+  background.setTextColor(conn_color, conn_color);
   background.drawString(conn ? "Current server IP" : "Server without connection", 48, 32, 1);
 
-  logoSprite.createSprite(41, 24);
-  logoSprite.pushImage(0, 0, 41, 24, minilogo);
-  logoSprite.pushToSprite(&background, 266, 13, TFT_BLACK);
+  logo_sprite.createSprite(41, 24);
+  logo_sprite.pushImage(0, 0, 41, 24, minilogo);
+  logo_sprite.pushToSprite(&background, 266, 13, TFT_BLACK);
 
-  String printChannels = "";
+  String print_channels = "";
   int chann_size = sizeof(channels) / sizeof(channels[0]);
   for (int i = 0; i < chann_size; i++) {
-    printChannels += "#" + channels[i] + (chann_size - 1 == i ? "" : ", ");
+    print_channels += "#" + channels[i] + (chann_size - 1 == i ? "" : ", ");
   }
 
   background.setTextColor(APP_GRAY, APP_GRAY);
@@ -142,9 +142,9 @@ void dashboard(){
   background.setTextColor(TFT_BLACK, TFT_BLACK);
   background.drawString(WIFI_SSID, 70, 56, 2);
   background.drawString(String(IRC_PORT), 70, 73, 2);
-  background.drawString(getMemoryUsage(), 70, 90, 2);
+  background.drawString(get_memory_usage(), 70, 90, 2);
   background.drawString(String(clients), 70, 107, 2);
-  background.drawString(printChannels, 70, 124, 2);
+  background.drawString(print_channels, 70, 124, 2);
 
   background.pushSprite(0,0);
 }
@@ -155,10 +155,10 @@ void setup(void) {
   tft.setRotation(1);
   tft.setSwapBytes(true);
 
-  splashScreen();
-  connectWifi();
+  splash_screen();
+  wifi_connect();
   delay(2000);
-  logoSprite.deleteSprite();
+  logo_sprite.deleteSprite();
   background.deleteSprite();
 }
 
